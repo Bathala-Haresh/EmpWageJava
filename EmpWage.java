b8495c55fb558e4d6ac08dd65b70c4a1cf54b3c4
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-02 01:00:36 +0530

    welcome to empwage

--- EmpWage.java
+++ EmpWage.java
@@ -4,28 +4,10 @@
 * @author  Bathala Haresh
 */
 
- public class EmpWage
+ class EmpWage
 {
     public static void main(String args[])
     {
-        //constants
-        final int fullTime = 1;
-        final int wageHr = 20;
-        
-        int empType = (int) (Math.random() * 100) % 2;
-        int workingHours = 0;
-        
-        //If condition to check employee is present or absent
-        if (empType == fullTime)
-        {
-            System.out.println("Employee is Present so ...");
-            workingHours = 8;
-        } else
-        {
-            System.out.println("Employee is Absent so ...");
-        }
-        //calculating the wage
-        int wage = workingHours * wageHr;
-        System.out.println("Employee Daily Wage is " + wage);
+        System.out.print("Welcome to Employee wage Computation program");
     }
 }
