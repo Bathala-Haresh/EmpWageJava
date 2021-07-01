ea0658c88a04234991ccbde46a4ff1b33af97b69
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-02 01:03:50 +0530

    calculating parttime employee and wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,13 +1,38 @@
 /**
-* This program is to calculate employee wage
+* This program is to add partTime employee and wage
 *
 * @author  Bathala Haresh
 */
 
- class EmpWage
+ public class EmpWage
 {
     public static void main(String args[])
     {
-        System.out.print("Welcome to Employee wage Computation program");
+        //constants
+        final int fullTime = 1;
+        final int partTime = 2;
+        final int wageHr = 20;
+        
+        //random method to get values like 0 or 1 or 2
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours = 0;
+        
+
+        if (empType == fullTime)
+        {
+            System.out.println("Employee is Present so ");
+            workingHours = 8;
+        } 
+        else if (empType == partTime) 
+        {
+            System.out.println("Employee has present PartTime so ");
+            workingHours = 4;
+        }else
+        {
+            System.out.println("Employee is Absent so ");
+        }
+        //calculate the wage of an employee
+        int wage = workingHours * wageHr;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
